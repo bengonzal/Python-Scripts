21160f0d23f238a1d828ee959a72ce25624fc58f
Benjamin Gonzalez Jr. <bengonzalez2688+github@gmail.com>
2023-11-02 14:45:32 -0700

    running into same issue with before_image var

--- img.py
+++ img.py
@@ -1,18 +1,22 @@
 import tkinter as tk
 from tkinter import filedialog
-from PIL import Image, ImageDraw, ImageFont, ImageTk
+from PIL import Image, ImageTk, ImageDraw, ImageFont
 
-ASCII_CHARS = "@%#*+=-:. "  
+ASCII_CHARS = "@%#*+=-:. "
+
+before_image = None
+after_image = None
 
 # open a file browser to select an image
-def open_image(): 
+def open_image():
+    global before_image
     file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.png *.jpg *.jpeg *.gif *.bmp *.ppm *.pgm")])
     try:
-        if file_path: 
-            image = Image.open(file_path)
-            photo = ImageTk.PhotoImage(image)
+        if file_path:
+            before_image = Image.open(file_path)
+            photo = ImageTk.PhotoImage(before_image)
             before_image_label.config(image=photo)
-            before_image_label.image = photo 
+            before_image_label.image = photo
     except Exception as e:
         print(e)
         return
@@ -25,12 +29,12 @@ def save_file(image):
 
 def scale_image(image, new_width=100):
     (original_width, original_height) = image.size
-    aspect_ratio = original_height/float(original_width)
+    aspect_ratio = original_height / float(original_width)
     new_height = int(aspect_ratio * new_width)
     new_image = image.resize((new_width, new_height))
     return new_image
 
-def apply_grayscale_filter(image): 
+def apply_grayscale_filter(image):
     # Convert image to grayscale
     grayscale_image = image.convert("L")
     return grayscale_image
@@ -45,7 +49,7 @@ def apply_ascii_filter(image, new_width=100):
     for i in range(height):
         for j in range(width):
             pixel_value = image.getpixel((j, i))
-            ascii_str += ASCII_CHARS[int(pixel_value // 35)] # Map grayscale value to ASCII chars
+            ascii_str += ASCII_CHARS[int(pixel_value // font_size_slider.get())]  # Map grayscale value to ASCII chars
         ascii_str += "\n"
     return ascii_str
 
@@ -62,21 +66,20 @@ def create_ascii_image(ascii_str, font_size):
     for i, line in enumerate(lines):
         for j, char in enumerate(line):
             position = (j * font_size, i * font_size)
-            draw.text(position, char, (0, 0, 0), font=font) 
+            draw.text(position, char, (0, 0, 0), font=font)
 
-    photo = ImageTk.PhotoImage(image)
-    after_image_label.config(image=photo)
-    after_image_label.image = photo
+    return image, image_width, image_height
 
-    return image
 
 def generate_ascii_image():
-    image = before_image_label.image # FIX THIS
-    ascii_str = apply_ascii_filter(image)
-    ascii_image = create_ascii_image(image, font_size_slider.get())
-    photo = ImageTk.PhotoImage(ascii_image)
-    after_image_label.config(ascii_image=photo)
-    after_image_label.image = photo 
+    global after_image
+    if before_image is not None:
+        ascii_str = apply_ascii_filter(before_image, 5) 
+        ascii_image, image_width, image_height = create_ascii_image(ascii_str, font_size_slider.get())
+        after_image = ascii_image
+        photo = ImageTk.PhotoImage(ascii_image)
+        after_image_label.config(image=photo, width=image_width, height=image_height)
+        after_image_label.image = photo
 
 
 # Main window
@@ -90,22 +93,22 @@ before_image_label.pack(side="left")
 after_image_label.pack(side="right")
 
 # create a button to select image/file_path
-open_button = tk.Button(root, text="Select Image", command=open_image) 
-open_button.pack() 
+open_button = tk.Button(root, text="Select Image", command=open_image)
+open_button.pack()
 
 # create a slider to select font size for ASCII image
 font_size_label = tk.Label(root, text="Font Size")
 font_size_label.pack()
-font_size_slider = tk.Scale(root, from_=4, to=100, orient= "horizontal")
-font_size_slider.set(16)    # initial font size
-font_size_slider.pack() 
+font_size_slider = tk.Scale(root, from_=4, to=100, orient="horizontal")
+font_size_slider.set(16)  # initial font size
+font_size_slider.pack()
 
-# create a button to generate ascii image 
-generate_ascii_image = tk.Button(root, text="Generate ASCII Image", command=generate_ascii_image)
-generate_ascii_image.pack()
+# create a button to generate ascii image
+generate_ascii_image_button = tk.Button(root, text="Generate ASCII Image", command=generate_ascii_image)
+generate_ascii_image_button.pack()
 
 # create a button to save ascii image
-save_button = tk.Button(root, text="Save Image", command=save_file)
+save_button = tk.Button(root, text="Save Image", command=save_file(after_image))
 save_button.pack()
 
 root.mainloop()
